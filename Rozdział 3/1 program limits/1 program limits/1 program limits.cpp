b62ad4c0aa9905a6b3740b165691cbf210aa95e3
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-08-16 21:56:27 +0200

    2 commit

--- Rozdział 3/1 program limits/1 program limits/1 program limits.cpp
+++ Rozdział 3/1 program limits/1 program limits/1 program limits.cpp
@@ -22,6 +22,6 @@ int main()
 
 	cout << "Minimalna wartosc typu int = " << INT_MIN << endl;
 	cout << "Bitów na bajt = " << CHAR_BIT << endl;
-	cout << "1 commit\n" << "2 commit";
+	cout << "1 commit\n" << "2 commit\n" << "3";
 	return 0;
 }

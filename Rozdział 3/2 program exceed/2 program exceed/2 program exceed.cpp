4d050bcf27b6dc5cc8c34ae73d4557293b16a32b
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-09-10 18:23:12 +0200

    Dodawanie 1 części programu

---
+++ Rozdział 3/2 program exceed/2 program exceed/2 program exceed.cpp
@@ -0,0 +1,23 @@
+﻿// 2 program exceed.cpp - przekroczenie ograniczeń typów całkowitych
+#include <iostream>
+#define ZERO 0              // niech ZERO oznacza wartosc 0
+#include <climits>          // definicja największej wartości int jako INT_MAX
+
+int main()
+{
+    using namespace std;
+
+    short sam = SHRT_MAX;       //inicjalizowanie zmiennej wartością maks
+    unsigned short sue = sam;   // mamy już zmienną, więc jest OK
+
+    cout << "Sam ma " << sam << " dolarow, a Sue ma " << sue;
+    cout << " dolatow." << endl
+         << "Do obu kont dodaj 1$. " << endl << "Teraz ";
+    sam = sam + 1;
+    sue = sue + 1;
+    cout << "Sam ma " << sam << " dolarow, a Sue ma " << sue;
+    cout << " dolarow.\nBiedny Sam!" << endl;
+    sam = ZERO;
+    sue = ZERO;
+    
+}

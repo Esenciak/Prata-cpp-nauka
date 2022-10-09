624756fffd485f02a2db82e625cfcb2b1fc39a4d
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-10-09 17:44:18 +0200

    Alarm Ben

---
+++ Rozdział 3/haker Ben/haker Ben/haker Ben.cpp
@@ -0,0 +1,14 @@
+﻿// haker Ben.cpp - alarm haker
+//
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    char alarm = '\a';
+    cout << alarm << "Nigdy wiecej tego nie rob!\a\n";
+    cout << "Byl tu\nhaker Ben \"Buggsie\"!\n";
+    return 0;
+}
+

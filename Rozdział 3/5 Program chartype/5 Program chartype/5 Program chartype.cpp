d3ef6a4de4203088ccd977a823a70f115593ab85
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-10-09 17:18:33 +0200

    dodaje 5 program

---
+++ Rozdział 3/5 Program chartype/5 Program chartype/5 Program chartype.cpp
@@ -0,0 +1,16 @@
+﻿// 5 Program chartype.cpp - typ char
+#include <iostream>
+
+
+
+int main()
+{
+	using namespace std;
+	char ch;				// deklaracja zmiennej typu char
+
+	cout << "Podaj znak: " << endl;
+	cin >> ch;
+	cout << "Holla! ";
+	cout << "Dziekuje za znak " << ch << "." << endl;
+	return 0;
+}

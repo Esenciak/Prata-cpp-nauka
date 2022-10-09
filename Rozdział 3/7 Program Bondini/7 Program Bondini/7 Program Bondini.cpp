32d700a45cf2535125f15612643068ccef39de7c
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-10-09 17:55:20 +0200

    Nie działa wiecej dzwiekow

--- Rozdział 3/7 Program Bondini/7 Program Bondini/7 Program Bondini.cpp
+++ Rozdział 3/7 Program Bondini/7 Program Bondini/7 Program Bondini.cpp
@@ -8,6 +8,11 @@ int main()
 	cout << "Podaj swoj kod agenta: ___________\b\b\b\b\b\b\b\b\b\b\b";
 	long code;
 	cin >> code;
+	cout << "\b";
+	cout << "\b";
+	cout << "\b";
+	cout << "\b";
+	cout << "\b";
 	cout << "\aPodales " << code << "...\n";
 	cout << "\aKod poprawny! Prosze realizowac plan Z3!\n";
 	return 0;

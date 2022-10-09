e9b0c1d033de9402bdf1a1a856a67c74049ac62e
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-10-09 17:54:23 +0200

    Program 7 bondini

---
+++ Rozdział 3/7 Program Bondini/7 Program Bondini/7 Program Bondini.cpp
@@ -0,0 +1,15 @@
+﻿// 7 Program Bondini.cpp - uzycie znakow specjalnych
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "\aOperacja \"HyperHype\" zostala uruchomiona!\n";
+	cout << "Podaj swoj kod agenta: ___________\b\b\b\b\b\b\b\b\b\b\b";
+	long code;
+	cin >> code;
+	cout << "\aPodales " << code << "...\n";
+	cout << "\aKod poprawny! Prosze realizowac plan Z3!\n";
+	return 0;
+}
+

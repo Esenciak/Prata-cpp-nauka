898ba307b46356fd8f418aa793a47ae982738970
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-10-09 17:28:33 +0200

    Dodaje program 6

---
+++ Rozdział 3/6 Program morechar/6 Program morechar/6 Program morechar.cpp
@@ -0,0 +1,25 @@
+﻿// 6 Program morechar.cpp - typ char a typ int
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	char ch = 'M';				// przypisanie zmiennej ch kodu ASCII znaku M
+	int i = ch;					// zapis tego samego kodu jako wartość int
+	cout << "Kod ASCII znaku " << ch << " to " << i << endl;
+
+	cout << "Dodajemy do kodu znaku jedynke:" << endl;
+	ch = ch + 1;				// zmiana kodu znaku ze zmiennej ch
+	i = ch;						// zapis nowego kodu w i
+	cout << "Kod ASCII znaku " << ch << " to " << i << endl;
+
+	// uzycie metodu cout.put() do pokazania znaku
+	cout << "Wyswietlenie char ch za pomoca cout.put(ch): ";
+	cout.put(ch);
+
+	//uzycie metody cout.put() do pokazania stalej znakowej
+	cout.put('!');
+
+	cout << endl << "Gotowe" << endl;
+	return 0;
+}

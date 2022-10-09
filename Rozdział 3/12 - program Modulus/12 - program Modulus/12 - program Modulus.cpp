d10402f7a9a692d9206f207c45044c2213cf25d4
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-10-09 23:01:45 +0200

    12 Program modulus

---
+++ Rozdział 3/12 - program Modulus/12 - program Modulus/12 - program Modulus.cpp
@@ -0,0 +1,17 @@
+﻿// 12 - program Modulus.cpp - uzywa operatora % do zamiany funtow na kamienie
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    const int Lbs_per_stn = 14;
+    int lbs;
+
+    cout << "Podaj swoja wage w funtach ";
+    cin >> lbs;
+    int stone = lbs / Lbs_per_stn;      // caly kamieni
+    int pounds = lbs % Lbs_per_stn;     // reszta w funtach
+    cout << lbs << " funtow to " << stone
+         << " kamieni, " << pounds << " funt(ow) .\n";
+    return 0;
+}

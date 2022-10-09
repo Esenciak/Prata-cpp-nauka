74cf0907b2869275f2241d0d49270cf23818c927
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-10-09 22:36:49 +0200

    10 Program arith

---
+++ Rozdział 3/10 Program arith/10 Program arith/10 Program arith.cpp
@@ -0,0 +1,22 @@
+﻿// 10 Program arith.cpp - arytmetyka C++
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    float hats, heads;
+
+    cout.setf(ios_base::fixed, ios_base::floatfield);       //fixed-point
+    cout << "Podaj liczbe: ------" << "\b\b\b\b\b\b";
+    cin >> hats;
+    cout << "Podaj druga liczbe: ------" << "\b\b\b\b\b\b";
+    cin >> heads;
+
+    cout << "hats = " << hats << " heads = " << heads << endl;
+    cout << "hats + heads = " << hats + heads << endl;
+    cout << "hats - heads = " << hats - heads << endl;
+    cout << "hats * heads = " << hats * heads << endl;
+    cout << "hats / heads = " << hats / heads << endl;
+    return 0;
+}
+

b9d36a18d4ef6430df0d3495d67d6e19f3185217
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-10-09 22:26:08 +0200

    Program 9

---
+++ Rozdział 3/9 Program fltadd/9 Program fltadd/9 Program fltadd.cpp
@@ -0,0 +1,17 @@
+﻿// 9 Program fltadd.cpp - problemy z dokladnoscia liczb float
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    float a = 2.34E+22f;
+    float b = a + 1.0f;
+
+    cout << "a = " << a << endl;
+    cout << "b - a = " << b - a << endl;
+
+    return 0;
+}
+
+
+// Float ma dokładność do 6/7 cyfr a nasze floaty mają ok 23 cyfry po przecinku dlatego dodatnie 1 i odjecie pierwotnej liczby nie daje 1 a 0

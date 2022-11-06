b46f0be01334ef50058d67299e19dcce9a1af385
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-06 23:03:42 +0100

    5 ćwiczenie

---
+++ Rozdział 3/Ćwiczenia/5 ćwiczenie/5 ćwiczenie/5 ćwiczenie.cpp
@@ -0,0 +1,16 @@
+﻿// 5 ćwiczenie.cpp - przeliczenie benzyny
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "Ile kilometrow przejechales: ";
+    double km;
+    cin >> km;
+    cout << "\nIle litrow paliwa spaliles: ";
+    int paliwo;
+    cin >> paliwo;
+    cout << "\nZuzycie samochodu wynosi: " << paliwo/km * 100;
+}

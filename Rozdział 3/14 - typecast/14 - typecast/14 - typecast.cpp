2f8935d57b4ac46d31d9c8a45de980595f4a2a58
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-06 18:19:35 +0100

    Dodaje 14 program

---
+++ Rozdział 3/14 - typecast/14 - typecast/14 - typecast.cpp
@@ -0,0 +1,26 @@
+﻿// 14 - typecast.cpp - wymuszanie zmian typu
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int auks, bats, coots;
+    
+    // ponizsza instrukacja dodaje wartosc jako typ double,
+    // nastepnie konwertuje wynik na int
+    auks = 19.99 + 11.99;
+
+    // te instrukcje dodaja wartosc jako typ int
+    bats = (int)19.99 + (int)11.99;         // stara skladnia C
+    coots = int(19.99) + int(11.99);        // nowa skladnia C++
+    cout << "auks = " << auks << ", bats = " << bats;
+    cout << ", coots = " << coots << endl;
+
+    char ch = 'Z';
+    cout << "Kod odpowiadajacy znakowi " << ch << " to ";
+                                            // pokaz jako char
+    cout << int(ch) << endl;                // pokaz jako int
+    return 0;
+}
+

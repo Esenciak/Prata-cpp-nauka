ac5272ac362fb76d3a7975ea241f232c7df4d4fc
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-06 22:50:40 +0100

    dodaje 4 ćwiczenie

---
+++ Rozdział 3/Ćwiczenia/4 cwiczenie/4 cwiczenie/4 cwiczenie.cpp
@@ -0,0 +1,21 @@
+﻿// 4 cwiczenie.cpp - obliczenie czasu
+//
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    
+    const long sekwdniu = 86400;
+    const long x = 60;
+
+    cout << "Podaj liczbe sekund: ";
+    long sekundy;
+    cin >> sekundy;
+ 
+
+    cout << sekundy << " sekund = " << sekundy/sekwdniu << " dni, " << sekundy%sekwdniu/3600 << " godzin, " << sekundy%sekwdniu%3600/x << " minut, " ;
+    cout << sekundy % sekwdniu % 3600 % x << " sekund.";
+    return 0;
+}

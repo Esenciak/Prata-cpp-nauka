53158a081c02b1047e96bba4bb9b3e1b037d9f1c
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-06 23:22:31 +0100

    6 cwiczenie

---
+++ Rozdział 3/Ćwiczenia/6 ćwiczenie/6 ćwiczenie/6 ćwiczenie.cpp
@@ -0,0 +1,14 @@
+﻿// 6 ćwiczenie.cpp - zmiana z litr an galon
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    
+    cout << "Podaj spalanie benzyny na 100 km: ";
+    double spalanie;
+    cin >> spalanie;                                    // spalanie na 100
+   
+    cout << "\nZuzycie benzyny w milach na galon wynosi " << 62.14 / (spalanie / 3.785);
+
+}

902b3962d697cb3a1c71a741841a25794e3ede12
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-06 19:51:15 +0100

    Dodaje 1 ćwiczenie

---
+++ Rozdział 3/Ćwiczenia/1 cwiczenie/1 cwiczenie/1 cwiczenie.cpp
@@ -0,0 +1,22 @@
+﻿// 1 cwiczenie.cpp - Program konwertujacy cm an stopy i cale ( 1 cal to 25.4 mm)
+
+#include <iostream>
+
+
+int main()
+{
+	using namespace std;
+
+	const long przeli = 25.4;
+
+	int centy;
+	cout << "Podaj cm: ____\b\b\b\b";
+	cin >> centy;
+	int stopy;
+	stopy = centy / przeli;
+	cout << "\n" << centy << " centymetrow to " << stopy << " stop";
+	int cale;
+	cale = centy % przeli;
+	cout << " _ " << cale << endl;
+	return 0;
+}

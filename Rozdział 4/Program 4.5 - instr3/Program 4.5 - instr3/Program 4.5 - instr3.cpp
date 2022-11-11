6c08bd92c6845bea84216ef6b2fad496b6385df7
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-11 20:13:37 +0100

    Dodaje program 4.5

---
+++ Rozdział 4/Program 4.5 - instr3/Program 4.5 - instr3/Program 4.5 - instr3.cpp
@@ -0,0 +1,21 @@
+﻿// Program 4.5 - instr3.cpp : Wczytywanie weicej niz jednego slowa za pomoca get()
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Podaj swoje imie:\n";
+    cin.get(name, ArSize).get();        //wczytanie wiersza i znaku nowego wiersza
+    cout << "Podaj swoj ulubiony deser:\n";
+    cin.get(dessert, ArSize).get();
+    cout << "Mam dla ciebie " << dessert;
+    cout << ", " << name << ".\n";
+    return 0;
+
+}
+

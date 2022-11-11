104f1f470f11d3648dea2c237c9ef7694cdc7530
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-11 20:44:39 +0100

    Dodaje program 4.7

---
+++ Rozdział 4/Program 4.7 - strtype1/Program 4.7 - strtype1/Program 4.7 - strtype1.cpp
@@ -0,0 +1,29 @@
+﻿// Program 4.7 - strtype1.cpp : uzycie klasy C++ string
+#include <iostream>
+#include <string>                           //udostepnienie dlasy string
+
+int main()
+{
+    using namespace std;
+    char charr1[20];                    // utworzenie pustej tablicy
+    char charr2[20] = "jaguar";         // utworzenie tablicy zainicjowanej
+    string str1;                        // utworzenie pustego obiektu string
+    string str2 = "pantera";            // utowrzenie zainicjalizowanego obiektu string
+
+
+    cout << "Podaj gatunek kota: ";
+    cin >> charr1;
+    cout << "\nPodaj inny gatunek kota: ";
+    cin >> str1;                        // wprowadzenie danych za pomocą cin
+    cout << "\nOto wybrane kotowate:\n";
+    cout << charr1 << " " << charr2 << " "
+         << str1 << " " << str2         // pokazywanie danych za pomoca cout
+         << endl;
+    cout << "Trzecia litera w slowie " << charr2 << " to "
+         << charr2[2] << endl;
+    cout << "Trzecia litera w slowie " << str2 << " to "
+         << str2[2] << endl;            // uzycie zapisu tablicowego
+    return 0;
+
+
+}

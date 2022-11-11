c628f92e7532e845027ee59ad5884c73a1273166
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-11 20:30:05 +0100

    Dodaje program 4.6 z celowym błędem

---
+++ Rozdział 4/Program 4.6 - numstr/Program 4.6 - numstr/Program 4.6 - numstr.cpp
@@ -0,0 +1,19 @@
+﻿// Program 4.6 - numstr.cpp : wczytywanie weirsza danych po liczbie
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "W ktorym roku zbudowano twoj dom?\n";
+    int year;
+    cin >> year;
+    cout << "Przy jakiej ulicy mieszkasz?\n";
+    char addres[80];
+    cin.getline(addres, 80);
+    cout << "Rok budowy: " << year << endl;
+    cout << "Addres: " << addres << endl;
+    cout << "\nGotowe!\a";
+    return 0;
+}
+

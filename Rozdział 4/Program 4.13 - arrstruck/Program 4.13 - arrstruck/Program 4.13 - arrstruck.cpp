3eb965f90966e52173b8b9a3486aba6a53612d55
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-13 16:07:53 +0100

    Dodaje program 4.13

---
+++ Rozdział 4/Program 4.13 - arrstruck/Program 4.13 - arrstruck/Program 4.13 - arrstruck.cpp
@@ -0,0 +1,26 @@
+﻿// Program 4.13 - arrstruck.cpp : tablica struktur
+
+#include <iostream>
+
+struct inflatable
+{
+    char name[20];
+    float volume;
+    double price;
+};
+
+int main()
+{
+    using namespace std;
+    inflatable guest[2] =                           //inicjalizacja tablicy struktury
+    {
+        {"Bambi", 0.5, 21.99},          //pierwsza struktura z tablicy
+        {"Godzilla", 2000, 565.99}      //nastepna struktura z tablicy
+    };
+
+    cout << "Goscie tacy jak " << guest[0].name << " i " << guest[1].name
+        << "\nrazem maja "
+        << guest[0].volume + guest[1].volume << " stop szesciennych.\n";
+    return 0;
+}
+

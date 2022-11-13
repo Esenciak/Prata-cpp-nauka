8bf5b56d7633692fde5698a925e0010bbab8966e
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-13 15:53:09 +0100

    Dodaje program 4.12

---
+++ Rozdział 4/Program 4.12 - assgn_st/Program 4.12 - assgn_st/Program 4.12 - assgn_st.cpp
@@ -0,0 +1,33 @@
+﻿// Program 4.12 - assgn_st.cpp : przypisywanie struktur
+
+#include <iostream>
+
+struct inflatable
+{
+    char name[20];
+    float volume;
+    double price;
+};
+
+int main()
+{
+    using namespace std;
+
+    inflatable bouguet =
+    {
+        "Slonecznik",
+        0.20,
+        12.49
+    };
+
+    inflatable choice;
+    cout << "zmienna bouguet: " << bouguet.name << " za ";
+    cout << bouguet.price << " zl\n";
+
+    choice = bouguet;           //przypisanie jednej struktury do drugiej  
+    cout << "zmienna choice: " << choice.name << " za ";
+    cout << choice.price << " zl\n";
+    return 0;
+}
+
+

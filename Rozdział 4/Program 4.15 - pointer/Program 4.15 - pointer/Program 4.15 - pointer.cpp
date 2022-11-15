1c15e8faafedc700c98fa5aea8f6bbcd73a921ae
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-15 19:50:29 +0100

    dodaje program 4.16

---
+++ Rozdział 4/Program 4.15 - pointer/Program 4.15 - pointer/Program 4.15 - pointer.cpp
@@ -0,0 +1,25 @@
+﻿// Program 4.15 - pointer.cpp : nasza pierwsza zmienna wskaznikowa
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int updates = 6;            //deklaracja zmiennej
+    int* p_updates;             //deklaracja wskaznika na int
+
+    p_updates = &updates;       //przypisanie wskaznikowi adresu int
+
+ //pokazanie wartosci na dwa sposoby
+    cout << "Wartosci: updates = " << updates;
+    cout << ", p_updates = " << p_updates << endl;
+
+ //pokazanie adresu na dwa sposoby
+    cout << "Adresy: &updates = " << &updates;
+    cout << ", p_updates = " << p_updates << endl;
+
+ //uzycie wskaznika do zmiany wartosci
+    *p_updates = *p_updates + 1;
+    cout << "Teraz updates = " << updates << endl;
+    return 0;
+}
+

f32a0ab40dbf3aa356d9cbd70ed0d47deaae1cf9
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-15 21:43:53 +0100

    dodaje program 4.18

---
+++ Rozdział 4/Program 4.18 - arreynew/Program 4.18 - arreynew/Program 4.18 - arreynew.cpp
@@ -0,0 +1,23 @@
+﻿// Program 4.18 - arreynew.cpp : uzycie operatora new do tablicy
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    double* p3 = new double[3];     // miejsce na 3 wartosci double
+    p3[0] = 0.2;                    // traktujemy p3 jak nazwe tablicy
+    p3[1] = 0.5;
+    p3[2] = 0.8;
+
+    cout << "p3[1] to " << p3[1] << " i p3 [0] to " << p3[0] << ".\n";
+
+    p3 = p3 + 1;                    // zwiekszenie wskaznika
+    cout << "Teraz p3[0] to " << p3[0] << " a ";
+    cout << "p3[1] to " << p3[1] << ".\n";
+    cout << " a p[2] to " << p3[0];
+
+    p3 = p3 - 1;                    // znowu wskazujemy poczatek tablicy
+    delete[] p3;                    // zwalniamy pamiec
+    return 0;
+}

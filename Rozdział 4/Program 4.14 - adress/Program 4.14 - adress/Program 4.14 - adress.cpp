d903aa1916b8f24097892500788c346b2c1653aa
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-15 19:43:40 +0100

    Dodaje program 4.14

---
+++ Rozdział 4/Program 4.14 - adress/Program 4.14 - adress/Program 4.14 - adress.cpp
@@ -0,0 +1,17 @@
+﻿// Program 4.14 - adress.cpp : uzycie operatora & do okreslenia adresu
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int donuts = 6;
+	double cups = 4.5;
+
+	cout << "Wartosci zmiennej donuts = " << donuts;
+	cout << ", a adres tej zmiennej = " << &donuts << endl;
+
+	cout << "Wartosc zmiennej cups = " << cups;
+	cout << ", a adres tej zmiennej = " << &cups << endl;
+	return 0;
+}

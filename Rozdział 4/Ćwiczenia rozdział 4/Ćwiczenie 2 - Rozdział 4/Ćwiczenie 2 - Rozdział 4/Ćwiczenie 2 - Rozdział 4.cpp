adfd4eb0cd0b77d76c75e1b4f369d7f6f95d111b
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-23 14:05:55 +0100

    Dodaje ćwiczenie 2

--- Rozdział 4/Ćwiczenia rozdział 4/Ćwiczenie 2 - Rozdział 4/Ćwiczenie 2 - Rozdział 4/Ćwiczenie 2 - Rozdział 4.cpp
+++ Rozdział 4/Ćwiczenia rozdział 4/Ćwiczenie 2 - Rozdział 4/Ćwiczenie 2 - Rozdział 4/Ćwiczenie 2 - Rozdział 4.cpp
@@ -1,20 +1,20 @@
-﻿// Ćwiczenie 2 - Rozdział 4.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿// Ćwiczenie 2 - Rozdział 4 : Przepisz listening 4.4 ale z string
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    using namespace std;
+    cout << "Podaj swoje imie: ";
+    string name;
+    getline(cin, name);
+    cout << "\nPodaj swoj ulubiony deser: ";
+    string desser;
+    getline(cin, desser);
+    cout << "\nMam dla ciebie " << desser << ", " << name;
+    
+    
+    return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln

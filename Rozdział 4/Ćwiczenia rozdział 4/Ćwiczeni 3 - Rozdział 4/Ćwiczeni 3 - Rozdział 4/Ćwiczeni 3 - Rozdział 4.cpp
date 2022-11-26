ff3d0f061ffaf02dca2deb23ae61977096ccb900
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-26 18:10:41 +0100

    Dodaje 3 ćwiczenie - 4 rozdział

---
+++ Rozdział 4/Ćwiczenia rozdział 4/Ćwiczeni 3 - Rozdział 4/Ćwiczeni 3 - Rozdział 4/Ćwiczeni 3 - Rozdział 4.cpp
@@ -0,0 +1,19 @@
+﻿// Ćwiczeni 3 - Rozdział 4.cpp : 
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    using namespace std;
+    const int Dane = 20;
+    char name[Dane];
+    char lastname[Dane];
+
+    cout << "Podaj swoje imie: ";
+    cin.getline(name,Dane);
+    cout << "\nPodaj swoje nazwisko: ";
+    cin.getline(lastname, Dane);
+    cout << "\n" << lastname << ", " << name;
+
+}

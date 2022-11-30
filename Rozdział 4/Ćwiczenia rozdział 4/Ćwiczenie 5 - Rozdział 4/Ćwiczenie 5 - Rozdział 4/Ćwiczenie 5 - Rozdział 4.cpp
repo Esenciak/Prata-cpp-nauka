36524ccdb646bf7526193c139746a163ebdd9743
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-30 16:22:39 +0100

    Dodaje Ćwiczenie 5 - Rozdział 4

---
+++ Rozdział 4/Ćwiczenia rozdział 4/Ćwiczenie 5 - Rozdział 4/Ćwiczenie 5 - Rozdział 4/Ćwiczenie 5 - Rozdział 4.cpp
@@ -0,0 +1,24 @@
+﻿// Ćwiczenie 5 - Rozdział 4.cpp : struktura batonik
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    struct baton 
+    {
+        string marka;
+        double waga;
+        int kalorie;
+    };
+
+    baton snack = { "Mocha Munch", 2.3, 350};
+
+    cout << "Nazwa marki: " << snack.marka << endl;
+    cout << "Waga batona: " << snack.waga << endl;
+    cout << "Kalorycznosc batona: " << snack.kalorie << endl;
+
+    return 0;
+
+}

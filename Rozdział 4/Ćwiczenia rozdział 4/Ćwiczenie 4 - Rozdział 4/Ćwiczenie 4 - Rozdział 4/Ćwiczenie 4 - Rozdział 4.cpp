c815daa75d9cb226cd65b3190f291ce22ae09762
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-30 16:14:32 +0100

    Dodaje ćwiczeni 4 - Rozdział 4

---
+++ Rozdział 4/Ćwiczenia rozdział 4/Ćwiczenie 4 - Rozdział 4/Ćwiczenie 4 - Rozdział 4/Ćwiczenie 4 - Rozdział 4.cpp
@@ -0,0 +1,20 @@
+﻿// Ćwiczenie 4 - Rozdział 4.cpp : ćwiczeni 3 ale string
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    using namespace std;
+
+    string imie;
+    cout << "Podaj imie: ";
+    cin >> imie;
+    string nazwisko;
+    cout << "\nPodaj nazwisko: ";
+    cin >> nazwisko;
+    nazwisko += ", " + imie;
+    cout << "Oto informacje nestawione w jeden napis: " << nazwisko;
+    return 0;
+}
+

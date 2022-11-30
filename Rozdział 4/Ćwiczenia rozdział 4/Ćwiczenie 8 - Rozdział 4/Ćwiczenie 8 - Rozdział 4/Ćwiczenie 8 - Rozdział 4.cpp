07705ee05303d15cccf3af35181899c2755eee0c
Karol Jabłoński <Karol.jablonski00@gmail.com>
2022-11-30 21:17:05 +0100

    Dodaje Ćwiczenie 8 - Rozdział 4

---
+++ Rozdział 4/Ćwiczenia rozdział 4/Ćwiczenie 8 - Rozdział 4/Ćwiczenie 8 - Rozdział 4/Ćwiczenie 8 - Rozdział 4.cpp
@@ -0,0 +1,31 @@
+﻿// Ćwiczenie 7 - Rozdział 4.cpp : Pizza time!!
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+
+    struct pizza
+    {
+        string name;
+        double srednica;
+        double waga;
+    };
+
+    cout << "Prosze podac srednice pizzy: ";
+    pizza* pizz = new pizza;
+    cin >> pizz->srednica;
+    cin.get();
+    cout << "Prosze podac nazwe firmy produkujaca pizze: ";
+    getline(cin, pizz->name);
+    cout << "Prosze podac wage pizzy: ";
+    cin >> pizz->waga;
+    cout << "\nOto wprowadzone dane: \n\n";
+    cout << "Srednica pizzy: " << pizz->srednica << endl;
+    cout << "Nazwa firmy: " << pizz->name << endl;
+    cout << "Waga pizzy: " << pizz->waga;
+    delete pizz;
+    
+    return 0;
+}
